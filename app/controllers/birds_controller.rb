65c3329f32c7329ca47b8923686d5328feefbd56
Stellanyambura <stellagitonga.com>
2023-03-15 19:18:01 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,6 +5,26 @@ class BirdsController < ApplicationController
     bird = Bird.create(name: params[:name], species: params[:species])
   render json: bird, status: :created
   end
-
-  # etc
+# PATCH /birds/:id
+def update
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(bird_params)
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+end
+def bird_params
+  params.permit(:name, :species, :likes)
+end
+def increment_likes
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+end
 end
